f8c72043b581614a4a65fe9267005369d8da2555
qfdox <110392544+qfdox@users.noreply.github.com>
2020-06-01 16:13:47 +0000

    - small fixes

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/HiltTestRunner.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/HiltTestRunner.kt
@@ -1,1 +1,8 @@
-package com.nemesis
+package com.nemesisprotocol.cryptocraze
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class Hilt
