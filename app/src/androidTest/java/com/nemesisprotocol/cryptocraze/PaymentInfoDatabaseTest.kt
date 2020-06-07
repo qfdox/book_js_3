1f0033b48c7bff08e3c97aa69719b6d3166a949b
qfdox <110392544+qfdox@users.noreply.github.com>
2020-06-07 15:41:09 +0000

    Correctif

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/PaymentInfoDatabaseTest.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/PaymentInfoDatabaseTest.kt
@@ -65,3 +65,86 @@ class PaymentInfoDatabaseTest {
         fiatWalletCard = FiatWalletCard(2415634636346, "User1", 5151, 123, 12134.00)
         paymentInfoDao.updateFiatWallet(fiatWalletCard)
         fiatWallets = paymentInfoDao.getFiatWallets()
+        Truth.assertThat(fiatWallets[0].balance).isEqualTo(12134.00)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_deleteFiatWallet() = runBlockingTest {
+        val fiatWalletCard = FiatWalletCard(2415634636346, "User1", 5151, 123, 53620.00)
+        paymentInfoDao.addFiatWallet(fiatWalletCard)
+        var fiatWallets = paymentInfoDao.getFiatWallets()
+        Truth.assertThat(fiatWallets.size).isEqualTo(1)
+        paymentInfoDao.deleteFiatWallet(fiatWalletCard)
+        fiatWallets = paymentInfoDao.getFiatWallets()
+        Truth.assertThat(fiatWallets.size).isEqualTo(0)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_getFiatWalletByCardNumber() = runBlockingTest {
+        val fiatWalletCard1 = FiatWalletCard(2415634636346, "User1", 5151, 123, 53620.00)
+        val fiatWalletCard2 = FiatWalletCard(5151515211255, "User2", 1512, 451, 12124.00)
+        paymentInfoDao.addFiatWallet(fiatWalletCard1)
+        paymentInfoDao.addFiatWallet(fiatWalletCard2)
+        val fiatWallets = paymentInfoDao.getFiatWallets()
+        Truth.assertThat(fiatWallets.size).isEqualTo(2)
+        val fetchedFiatWalletByCardNumber = paymentInfoDao.getFiatWalletByCardNumber(2415634636346)
+        Truth.assertThat(fetchedFiatWalletByCardNumber).isEqualTo(fiatWalletCard1)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_addCryptoCrazeVisaCard() = runBlockingTest {
+        val cryptoCrazeVisaCard =
+            CryptoCrazeVisaCard(123456789, 5151.6326, CryptoCrazeVisaColour.RED)
+        paymentInfoDao.addCryptoCrazeVisaCard(cryptoCrazeVisaCard)
+        val cryptoCrazeVisaCards = paymentInfoDao.getCryptoCrazeVisaCards()
+        Truth.assertThat(cryptoCrazeVisaCards).contains(cryptoCrazeVisaCard)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_updateCryptoCrazeVisaCard() = runBlockingTest {
+        var cryptoCrazeVisaCard =
+            CryptoCrazeVisaCard(123456789, 5151.12, CryptoCrazeVisaColour.RED)
+        paymentInfoDao.addCryptoCrazeVisaCard(cryptoCrazeVisaCard)
+        var cryptoCrazeVisaCards = paymentInfoDao.getCryptoCrazeVisaCards()
+        Truth.assertThat(cryptoCrazeVisaCards[0].cardId).isEqualTo(123456789)
+        Truth.assertThat(cryptoCrazeVisaCards[0].balance).isEqualTo(5151.12)
+        cryptoCrazeVisaCard =
+            CryptoCrazeVisaCard(123456789, 12345.41, CryptoCrazeVisaColour.RED)
+        paymentInfoDao.updateCryptoCrazeVisaCard(cryptoCrazeVisaCard)
+        cryptoCrazeVisaCards = paymentInfoDao.getCryptoCrazeVisaCards()
+        Truth.assertThat(cryptoCrazeVisaCards[0].cardId).isEqualTo(123456789)
+        Truth.assertThat(cryptoCrazeVisaCards[0].balance).isEqualTo(12345.41)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_getCryptoCrazeVisaCardByCardId() = runBlockingTest {
+        val cryptoCrazeVisaCard1 =
+            CryptoCrazeVisaCard(123456789, 5151.23, CryptoCrazeVisaColour.RED)
+        val cryptoCrazeVisaCard2 =
+            CryptoCrazeVisaCard(515664366, 1516.32, CryptoCrazeVisaColour.RED)
+        paymentInfoDao.addCryptoCrazeVisaCard(cryptoCrazeVisaCard1)
+        paymentInfoDao.addCryptoCrazeVisaCard(cryptoCrazeVisaCard2)
+        val cryptoCrazeVisaCards = paymentInfoDao.getCryptoCrazeVisaCards()
+        Truth.assertThat(cryptoCrazeVisaCards.size).isEqualTo(2)
+        val fetchedCryptoVisaCardById = paymentInfoDao.getCryptoCrazeVisaCardByCardId(515664366)
+        Truth.assertThat(fetchedCryptoVisaCardById).isEqualTo(cryptoCrazeVisaCard2)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_deleteCryptoCrazeVisaCard() = runBlockingTest {
+        val cryptoCrazeVisaCard =
+            CryptoCrazeVisaCard(123456789, 5151.6326, CryptoCrazeVisaColour.RED)
+        paymentInfoDao.addCryptoCrazeVisaCard(cryptoCrazeVisaCard)
+        var cryptoCrazeVisaCards = paymentInfoDao.getCryptoCrazeVisaCards()
+        Truth.assertThat(cryptoCrazeVisaCards.size).isEqualTo(1)
+        paymentInfoDao.deleteCryptoCrazeVisaCard(cryptoCrazeVisaCard)
+        cryptoCrazeVisaCards = paymentInfoDao.getCryptoCrazeVisaCards()
+        Truth.assertThat(cryptoCrazeVisaCards.size).isEqualTo(0)
+    }
+}
---
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/PortfolioDatabaseTest.kt
@@ -0,0 +1,90 @@
+
+package com.nemesisprotocol.cryptocraze
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth
+import com.nemesisprotocol.cryptocraze.data.database.portfolio.PortfolioDao
+import com.nemesisprotocol.cryptocraze.data.database.portfolio.PortfolioDatabase
+import com.nemesisprotocol.cryptocraze.domain.portfolio.CryptoInvestment
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltAndroidTest
+@SmallTest
+class PortfolioDatabaseTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    @Named("test_portfolio_db")
+    lateinit var database: PortfolioDatabase
+    private lateinit var portfolioDao: PortfolioDao
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        portfolioDao = database.portfolioDao()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_addCryptoInvestment() = runBlockingTest {
+        val cryptoInvestment = CryptoInvestment("BTC", "Bitcoin", 6.0)
+        portfolioDao.addCryptoInvestment(cryptoInvestment)
+        val portfolio = portfolioDao.getPortfolio()
+        Truth.assertThat(portfolio).contains(cryptoInvestment)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_updateCryptoInvestment() = runBlockingTest {
+        var cryptoInvestment = CryptoInvestment("BTC", "Bitcoin", 6.0)
+        portfolioDao.addCryptoInvestment(cryptoInvestment)
+        var portfolio = portfolioDao.getPortfolio()
+        Truth.assertThat(portfolio[0].cryptoAmount).isEqualTo(6.0)
+        cryptoInvestment = CryptoInvestment("BTC", "Bitcoin", 2.0)
+        portfolioDao.updateCryptoInvestment(cryptoInvestment)
+        portfolio = portfolioDao.getPortfolio()
+        Truth.assertThat(portfolio[0].cryptoAmount).isEqualTo(2.0)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_deleteCryptoInvestment() = runBlockingTest {
+        val cryptoInvestment = CryptoInvestment("BTC", "Bitcoin", 6.0)
+        portfolioDao.addCryptoInvestment(cryptoInvestment)
+        var portfolio = portfolioDao.getPortfolio()
+        Truth.assertThat(portfolio.size).isEqualTo(1)
+        portfolioDao.deleteCryptoInvestment(cryptoInvestment)
+        portfolio = portfolioDao.getPortfolio()
+        Truth.assertThat(portfolio.size).isEqualTo(0)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun test_checkCryptoIsInvested() = runBlockingTest {
+        val cryptoInvestment = CryptoInvestment("BTC", "Bitcoin", 6.0)
+        portfolioDao.addCryptoInvestment(cryptoInvestment)
+        var isCryptoInvested = portfolioDao.checkCryptoIsInvested("BTC")
+        Truth.assertThat(isCryptoInvested).isEqualTo(true)
+        isCryptoInvested = portfolioDao.checkCryptoIsInvested("ETH")
+        Truth.assertThat(isCryptoInvested).isEqualTo(false)
+    }
