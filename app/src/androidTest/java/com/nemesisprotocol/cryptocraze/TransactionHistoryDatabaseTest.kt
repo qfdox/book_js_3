b39f6288425cce8ff17dca5f5eaafe557d8b664c
qfdox <110392544+qfdox@users.noreply.github.com>
2020-06-08 15:00:14 +0000

    rep

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/TransactionHistoryDatabaseTest.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/TransactionHistoryDatabaseTest.kt
@@ -2,4 +2,6 @@ package com.nemesisprotocol.cryptocraze
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.SmallTest
-import com.google
+import com.google.common.truth.Truth
+import com.nemesisprotocol.cryptocraze.data.database.transactionhistory.TransactionHistoryDao
+import com.nemesisprotocol.cryptocraze.data.database.transact
