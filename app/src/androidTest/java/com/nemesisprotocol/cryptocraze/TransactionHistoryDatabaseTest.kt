36253a721deca1708769982721444a339807c719
qfdox <110392544+qfdox@users.noreply.github.com>
2020-06-12 13:13:16 +0000

    v1.0.0 release

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/TransactionHistoryDatabaseTest.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/TransactionHistoryDatabaseTest.kt
@@ -4,4 +4,6 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth
 import com.nemesisprotocol.cryptocraze.data.database.transactionhistory.TransactionHistoryDao
-import com.nemesisprotocol.cryptocraze.data.database.transact
+import com.nemesisprotocol.cryptocraze.data.database.transactionhistory.TransactionHistoryDatabase
+import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionRecord
+import com.nemesisprotocol.cryptocra
