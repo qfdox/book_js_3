a19e2f3242cece6dfc5011b47566fbcaace7dde7
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-02 18:04:21 +0000

    Update Chrome.md

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/UserDatabaseTest.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/UserDatabaseTest.kt
@@ -1,4 +1,7 @@
 package com.nemesisprotocol.cryptocraze
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.nemesisprotocol.cryptocraze.data.database.user.UserDao
+import com.nemesisprotocol.cryptocraze.data.database.user.User
