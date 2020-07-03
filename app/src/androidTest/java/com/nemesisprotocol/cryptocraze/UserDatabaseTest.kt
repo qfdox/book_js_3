3ba3b1e307897915fb580165b52468361a389be8
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-03 15:37:39 +0000

    0.3.10

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/UserDatabaseTest.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/UserDatabaseTest.kt
@@ -4,4 +4,8 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth.assertThat
 import com.nemesisprotocol.cryptocraze.data.database.user.UserDao
-import com.nemesisprotocol.cryptocraze.data.database.user.User
+import com.nemesisprotocol.cryptocraze.data.database.user.UserDatabase
+import com.nemesisprotocol.cryptocraze.domain.user.User
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+imp
