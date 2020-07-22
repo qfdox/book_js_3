98cc8d31ef1245cee2e9e4341a90b3ea824913a9
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-22 10:34:21 +0000

    Fix e2e tests

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/di/TestAppModule.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/di/TestAppModule.kt
@@ -4,4 +4,6 @@ import android.content.Context
 import androidx.room.Room
 import com.nemesisprotocol.cryptocraze.data.database.cryptofavdata.CryptoFavDataDatabase
 import com.nemesisprotocol.cryptocraze.data.database.paymentinfo.PaymentInfoDatabase
-import co
+import com.nemesisprotocol.cryptocraze.data.database.portfolio.PortfolioDatabase
+import com.nemesisprotocol.cryptocraze.data.database.transactionhistory.TransactionHistoryDatabase
+import com.nemesispro
