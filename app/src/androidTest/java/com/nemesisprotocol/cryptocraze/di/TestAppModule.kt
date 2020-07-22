8c22f694f6d9dbe72c7e20d9f2acc15d62a62c22
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-22 08:28:56 +0000

    }

--- app/src/androidTest/java/com/nemesisprotocol/cryptocraze/di/TestAppModule.kt
+++ app/src/androidTest/java/com/nemesisprotocol/cryptocraze/di/TestAppModule.kt
@@ -2,3 +2,6 @@ package com.nemesisprotocol.cryptocraze.di
 
 import android.content.Context
 import androidx.room.Room
+import com.nemesisprotocol.cryptocraze.data.database.cryptofavdata.CryptoFavDataDatabase
+import com.nemesisprotocol.cryptocraze.data.database.paymentinfo.PaymentInfoDatabase
+import co
