03b2b98be99140739c090dc71058bc605c0b492c
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-30 23:03:00 +0000

    Removed commented lines

---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/CryptoCrazeApplication.kt
@@ -0,0 +1,7 @@
+package com.nemesisprotocol.cryptocraze
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoCrazeApplication : Application()
---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/MainActivity.kt
@@ -0,0 +1,21 @@
+
+package com.nemesisprotocol.cryptocraze
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
