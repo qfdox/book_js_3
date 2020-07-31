febc5336fd0e747332763938c4547b71c72e40b3
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-31 13:50:07 +0000

    enhancements

--- app/src/main/java/com/nemesisprotocol/cryptocraze/Screen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/Screen.kt
@@ -1,1 +1,5 @@
-package com.neme
+package com.nemesisprotocol.cryptocraze
+
+sealed class Screen(val route: String, val icon: Int?, val title: String) {
+    object Splash : Screen("splash_screen", null, "Splash")
+    obj
