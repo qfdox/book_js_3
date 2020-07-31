aeecacc8bcac3da857831e3596c23f6fce9d3a82
qfdox <110392544+qfdox@users.noreply.github.com>
2020-07-31 16:56:49 +0000

    Add JavaDoc

--- app/src/main/java/com/nemesisprotocol/cryptocraze/Screen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/Screen.kt
@@ -2,4 +2,7 @@ package com.nemesisprotocol.cryptocraze
 
 sealed class Screen(val route: String, val icon: Int?, val title: String) {
     object Splash : Screen("splash_screen", null, "Splash")
-    obj
+    object Login : Screen("login_screen", null, "Login")
+    object SignUp : Screen("sign_up_screen", null, "Signup")
+    object Home : Screen("home_screen", R.drawable.ic_home_24, "Home")
+ 
