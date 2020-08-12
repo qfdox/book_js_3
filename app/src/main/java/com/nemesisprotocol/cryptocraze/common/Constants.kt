aa9da19463009bbeafbbb2ea3b9736017056dbb7
qfdox <110392544+qfdox@users.noreply.github.com>
2020-08-12 14:40:32 +0000

    ax: kweather   pts/7        Jun 29 09:35\t(celeste)

--- app/src/main/java/com/nemesisprotocol/cryptocraze/common/Constants.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/common/Constants.kt
@@ -1,4 +1,8 @@
 package com.nemesisprotocol.cryptocraze.common
 
 object Constants {
-    const val COIN_GECKO_BASE_URL = "https:
+    const val COIN_GECKO_BASE_URL = "https://api.coingecko.com/api/v3/"
+    const val COIN_PAPRIKA_BASE_URL = "https://api.coinpaprika.com/"
+
+    const val PARAM_COIN_ID = "coinId"
+}
---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/common/DispatcherProvider.kt
@@ -0,0 +1,1 @@
+package com.nemesisprotocol.cryptocraze.com
