04db23cc9120c84673033792eaf39954eef65d6d
qfdox <110392544+qfdox@users.noreply.github.com>
2020-08-18 14:43:14 +0000

    Update poll.js

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptodata/remote/CryptoDataApiMapper.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptodata/remote/CryptoDataApiMapper.kt
@@ -4,4 +4,6 @@
 * Copyright (c) 2020 Gurupreet Singh
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this so
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish
