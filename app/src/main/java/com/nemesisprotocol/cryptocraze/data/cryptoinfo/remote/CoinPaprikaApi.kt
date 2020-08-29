e1e9f846597d73b48f85c8f82d475d06eebb17e0
qfdox <110392544+qfdox@users.noreply.github.com>
2020-08-29 19:07:20 +0000

    Fix PEP8 issues

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/CoinPaprikaApi.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/CoinPaprikaApi.kt
@@ -1,1 +1,11 @@
-package com.nemesisprotocol.cryptocraze.data.cryptoi
+package com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinPaprikaApi {
+
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/
