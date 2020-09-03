d1a0fcfe4634f9f5781923fa1b757f673af84e96
qfdox <110392544+qfdox@users.noreply.github.com>
2020-09-03 18:44:04 +0000

    Add missing include file

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/CoinDto.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/CoinDto.kt
@@ -1,3 +1,10 @@
 package com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto
 
-import com.google.gson.annotations
+import com.google.gson.annotations.SerializedName
+import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.Coin
+
+data class CoinDto(
+    val id: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    @SerializedNa
