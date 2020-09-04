9bd00afebd873e4a2f56946f8bd6c03e952a3226
qfdox <110392544+qfdox@users.noreply.github.com>
2020-09-04 06:54:05 +0000

    Debugged

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/CoinDto.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/CoinDto.kt
@@ -7,4 +7,13 @@ data class CoinDto(
     val id: String,
     @SerializedName("is_active")
     val isActive: Boolean,
-    @SerializedNa
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
+
+fun CoinDto.toCoin(): Coin {
+    ret
