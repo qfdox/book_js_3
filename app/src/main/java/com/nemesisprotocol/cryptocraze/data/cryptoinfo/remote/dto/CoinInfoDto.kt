5a9eab54348fd5d494c4a9b1c38f53b2d3006027
qfdox <110392544+qfdox@users.noreply.github.com>
2020-09-06 16:48:07 +0000

    Delete main.cpp

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/CoinInfoDto.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/CoinInfoDto.kt
@@ -4,4 +4,10 @@ import com.google.gson.annotations.SerializedName
 import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.CoinDetail
 
 data class CoinInfoDto(
-    val description: String
+    val description: String,
+    @SerializedName("development_status")
+    val developmentStatus: String,
+    @SerializedName("first_data_at")
+    val firstDataAt: String,
+    @SerializedName("hardware_wallet")
+    
