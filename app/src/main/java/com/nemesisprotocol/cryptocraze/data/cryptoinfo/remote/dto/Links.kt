d0e071588f4b7cdc90535e4ee93a7742622e3d1d
qfdox <110392544+qfdox@users.noreply.github.com>
2020-09-14 15:34:25 +0000

    More debug output.

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/Links.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/cryptoinfo/remote/dto/Links.kt
@@ -3,4 +3,8 @@ package com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class Links(
-  
+    val explorer: List<String>,
+    val facebook: List<String>,
+    val reddit: List<String>,
+    @SerializedName("source_code")
+    val sourceCode: List<String
