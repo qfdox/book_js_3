fa0d5ae98cd3b0014e490955b09f7c11492000b3
qfdox <110392544+qfdox@users.noreply.github.com>
2020-10-09 12:04:51 +0000

    Update submodule.

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/cryptofavdata/CryptoFavDataDao.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/cryptofavdata/CryptoFavDataDao.kt
@@ -1,1 +1,8 @@
-package com.nemesisprotocol.cryptocraze.da
+package com.nemesisprotocol.cryptocraze.data.database.cryptofavdata
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transa
