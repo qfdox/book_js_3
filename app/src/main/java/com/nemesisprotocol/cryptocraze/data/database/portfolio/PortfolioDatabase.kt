a7f4b197a89146ee1727fca6e267c8665e5d1f73
qfdox <110392544+qfdox@users.noreply.github.com>
2020-10-11 20:30:58 +0000

    Blub

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/portfolio/PortfolioDatabase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/portfolio/PortfolioDatabase.kt
@@ -2,4 +2,8 @@ package com.nemesisprotocol.cryptocraze.data.database.portfolio
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.nemesisprotocol.cryptocraze.domain.portfolio.Cryp
+import com.nemesisprotocol.cryptocraze.domain.portfolio.CryptoInvestment
+
+@Database(entities = [CryptoInvestment::class], version = 2, exportSchema = false)
+abstract class PortfolioDatabase : RoomDatabase() {
+    abstract fun portfolioDao():
