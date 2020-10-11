9cd329440d3e32aadbe722546e146825debbad74
qfdox <110392544+qfdox@users.noreply.github.com>
2020-10-11 08:37:06 +0000

    Update mac.md

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/portfolio/PortfolioDao.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/portfolio/PortfolioDao.kt
@@ -1,1 +1,8 @@
-package com.nemesisprotocol.cryptocraze.data.database.port
+package com.nemesisprotocol.cryptocraze.data.database.portfolio
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.nemesisprotocol.crypto
