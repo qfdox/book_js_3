f6dbf44e2f4587df9d8581447bfc282f74aeb03f
qfdox <110392544+qfdox@users.noreply.github.com>
2020-10-12 21:35:33 +0000

    USB: add ftdi_sio USB ID for GDM Boost V1.x

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/user/UserDao.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/user/UserDao.kt
@@ -1,4 +1,14 @@
 package com.nemesisprotocol.cryptocraze.data.database.user
 
 import androidx.lifecycle.LiveData
-import androidx.roo
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.nemesisprotocol.cryptocraze.domain.user.User
+
+@Dao
+interface UserDao {
+
+    @Query("SELECT * FROM user")
+    fun g
