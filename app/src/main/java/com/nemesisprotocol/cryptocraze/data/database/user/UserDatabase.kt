8758f2b8253b1919481906786111d204a37cbdfb
qfdox <110392544+qfdox@users.noreply.github.com>
2020-10-15 20:01:09 +0000

    updating lists

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/user/UserDatabase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/database/user/UserDatabase.kt
@@ -1,1 +1,5 @@
-packag
+package com.nemesisprotocol.cryptocraze.data.database.user
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nemesisprotocol.cryptocraze.domain.use
