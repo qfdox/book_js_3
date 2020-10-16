a2f0db2ea9a784e797c4d845fe9ad92f196ab538
qfdox <110392544+qfdox@users.noreply.github.com>
2020-10-16 17:59:13 +0000

    msm: camera: Fix memory read by adding bounds check

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/di/DatabaseModule.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/di/DatabaseModule.kt
@@ -1,4 +1,7 @@
 package com.nemesisprotocol.cryptocraze.data.di
 
 import android.content.Context
-import androidx.room.Ro
+import androidx.room.Room
+import com.nemesisprotocol.cryptocraze.data.database.cryptofavdata.CryptoFavDataDao
+import com.nemesisprotocol.cryptocraze.data.database.cryptofavdata.CryptoFavDataDatabase
+import com.nemesis
