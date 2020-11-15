748a304915a033b7c23f574c725d079c478f31a2
qfdox <110392544+qfdox@users.noreply.github.com>
2020-11-15 21:32:59 +0000

    switch to travis

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/CoinRepoImpl.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/CoinRepoImpl.kt
@@ -1,4 +1,9 @@
 package com.nemesisprotocol.cryptocraze.data.repos
 
 import com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto.CoinDto
-import com.nemesisprotocol.cryptocraze.data.cryptoinfo.re
+import com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto.CoinInfoDto
+import com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto.CoinPaprikaApi
+import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.CoinRepo
+import javax.inject.Inject
+
+c
