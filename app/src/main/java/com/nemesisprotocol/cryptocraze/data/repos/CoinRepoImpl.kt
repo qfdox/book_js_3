9766924eb0721da896d59a60bf5ee7fb42a4c7ec
qfdox <110392544+qfdox@users.noreply.github.com>
2020-11-18 11:39:57 +0000

    Removed some commented code.

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/CoinRepoImpl.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/CoinRepoImpl.kt
@@ -6,4 +6,12 @@ import com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto.CoinPaprikaApi
 import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.CoinRepo
 import javax.inject.Inject
 
-c
+class CoinRepoImpl @Inject constructor(
+    private val api: CoinPaprikaApi
+) : CoinRepo {
+
+    override suspend fun getCoins(): List<CoinDto> {
+        return api.getCoins()
+    }
+
+    override suspend f
