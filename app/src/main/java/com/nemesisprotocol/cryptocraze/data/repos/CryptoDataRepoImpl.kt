b326eb2417098dd530926b0f2e3813e92198a5b1
qfdox <110392544+qfdox@users.noreply.github.com>
2020-11-21 18:10:53 +0000

    Revert "..."

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/CryptoDataRepoImpl.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/CryptoDataRepoImpl.kt
@@ -3,4 +3,9 @@ package com.nemesisprotocol.cryptocraze.data.repos
 import androidx.annotation.WorkerThread
 import com.nemesisprotocol.cryptocraze.data.cryptodata.remote.CryptoDataApi
 import com.nemesisprotocol.cryptocraze.data.cryptodata.remote.CryptoDataApiMapper
-import com.nemesisprotocol.cryptocraze.domain.cryptodata.Crypt
+import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoData
+import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoDataRepo
+import javax.inject.Inject
+
+class CryptoDataRepoImpl @Inject constructor(
+    private val cryptoApi: 
