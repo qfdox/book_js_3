cce6ea2ebd4a6f0b8177d5a961062b60b39daae2
qfdox <110392544+qfdox@users.noreply.github.com>
2020-12-08 14:13:44 +0000

    wut

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/PaymentInfoRepoImpl.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/PaymentInfoRepoImpl.kt
@@ -3,4 +3,8 @@ package com.nemesisprotocol.cryptocraze.data.repos
 import com.nemesisprotocol.cryptocraze.data.database.paymentinfo.PaymentInfoDao
 import com.nemesisprotocol.cryptocraze.domain.paymentinfo.CryptoCrazeVisaCard
 import com.nemesisprotocol.cryptocraze.domain.paymentinfo.FiatWalletCard
-import com.ne
+import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
+import javax.inject.Inject
+
+class PaymentInfoRepoImpl @Inject constructor(private val paymentInfoDao: PaymentInfoDao) :
+    Paymen
