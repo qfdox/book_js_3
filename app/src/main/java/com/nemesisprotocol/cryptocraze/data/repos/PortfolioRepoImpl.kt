2b642df824b682ec664ee5dc8a2c967a6c02349d
qfdox <110392544+qfdox@users.noreply.github.com>
2020-12-31 16:07:25 +0000

    package: new vcl meta properties

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/PortfolioRepoImpl.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/PortfolioRepoImpl.kt
@@ -2,4 +2,10 @@ package com.nemesisprotocol.cryptocraze.data.repos
 
 import com.nemesisprotocol.cryptocraze.data.database.portfolio.PortfolioDao
 import com.nemesisprotocol.cryptocraze.domain.portfolio.CryptoInvestment
-import com.nemesisprotocol.cryptocraze.domain.portfolio.PortfolioR
+import com.nemesisprotocol.cryptocraze.domain.portfolio.PortfolioRepo
+import javax.inject.Inject
+
+class PortfolioRepoImpl @Inject constructor(private val portfolioDao: PortfolioDao) :
+    PortfolioRepo {
+
+    override fun getPortfolio(): List<Crypt
