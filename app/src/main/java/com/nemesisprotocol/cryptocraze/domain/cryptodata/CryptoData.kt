7707caa77c8476ad225bc28494ab81334b20af2d
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-07 15:06:59 +0000

    mm: compaction: fix the page state calculation in too_many_isolated

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/CryptoData.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/CryptoData.kt
@@ -1,3 +1,10 @@
 package com.nemesisprotocol.cryptocraze.domain.cryptodata
 
-import andr
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "crypto_favorites")
+data class CryptoData(
+    @PrimaryK
