632657e52661afa0cc775990d62dd7ef96b4839f
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-07 08:38:35 +0000

    readahead: make context readahead more conservative

--- app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/TransactionHistoryRepoImpl.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/data/repos/TransactionHistoryRepoImpl.kt
@@ -2,4 +2,10 @@ package com.nemesisprotocol.cryptocraze.data.repos
 
 import com.nemesisprotocol.cryptocraze.data.database.transactionhistory.TransactionHistoryDao
 import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionHistoryRepo
-import com.nemesisprotocol.cryptocraze.domain.transactionhistory.Transac
+import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionRecord
+import javax.inject.Inject
+
+class TransactionHistoryRepoImpl @Inject constructor(private val transactionHistoryDao: TransactionHistoryDao) :
+    TransactionHistoryRepo {
+
+    override fun get
