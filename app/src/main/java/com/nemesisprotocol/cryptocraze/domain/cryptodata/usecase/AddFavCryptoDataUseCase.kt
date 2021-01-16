1509ad19bc12c2ffa82fc66b99241c44bd97e988
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-16 12:27:15 +0000

    simplifying

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/AddFavCryptoDataUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/AddFavCryptoDataUseCase.kt
@@ -1,4 +1,8 @@
 package com.nemesisprotocol.cryptocraze.domain.cryptodata.usecase
 
 import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoData
-import com.n
+import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoFavDataRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+impor
