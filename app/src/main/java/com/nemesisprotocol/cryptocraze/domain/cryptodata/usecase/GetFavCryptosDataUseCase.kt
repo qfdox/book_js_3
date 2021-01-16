10376937ce26924027f6cfc3a7ab67e78c28d463
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-16 14:57:43 +0000

    sort imports

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/GetFavCryptosDataUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/GetFavCryptosDataUseCase.kt
@@ -2,4 +2,7 @@ package com.nemesisprotocol.cryptocraze.domain.cryptodata.usecase
 
 import androidx.lifecycle.LiveData
 import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoData
-i
+import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoFavDataRepo
+import javax.inject.Inject
+
+class GetFavCryptosDataUseCase @Inject constructor(private val cryptoFavData
