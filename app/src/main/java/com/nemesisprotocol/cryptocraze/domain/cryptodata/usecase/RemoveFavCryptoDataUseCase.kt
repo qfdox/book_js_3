9a33692a4fc186364114df75ebc19b6c0b0b53ff
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-17 19:44:35 +0000

    bump to 0.4.1

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/RemoveFavCryptoDataUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/RemoveFavCryptoDataUseCase.kt
@@ -2,4 +2,9 @@ package com.nemesisprotocol.cryptocraze.domain.cryptodata.usecase
 
 import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoData
 import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoFavDataRepo
-import kotli
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class RemoveFavCryptoDataUseCase @Inject constructor(priva
