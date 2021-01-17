55a9192e63f30e9773a831722c64ab893297736b
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-17 19:20:24 +0000

    [Dev Deps] update `jscs`, `eslint`, `@ljharb/eslint-config`

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/RemoveFavCryptoDataUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptodata/usecase/RemoveFavCryptoDataUseCase.kt
@@ -1,1 +1,5 @@
-package com.nemesisprotocol.cryptocraze.domain.cryptodata.u
+package com.nemesisprotocol.cryptocraze.domain.cryptodata.usecase
+
+import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoData
+import com.nemesisprotocol.cryptocraze.domain.cryptodata.CryptoFavDataRepo
+import kotli
