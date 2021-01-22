77f7483d8f9eafc87d94dcf4944cfb5156c42472
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-22 20:08:11 +0000

    update gitignore

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptoinfo/CoinRepo.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/cryptoinfo/CoinRepo.kt
@@ -1,1 +1,8 @@
-package com.nemesisprotocol.cryptocraze.domain.cryp
+package com.nemesisprotocol.cryptocraze.domain.cryptoinfo
+
+import com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto.CoinDto
+import com.nemesisprotocol.cryptocraze.data.cryptoinfo.remote.dto.CoinInfoDto
+
+interface CoinRepo {
+
+  
