42d33c764b9de29811151c585b79d49853c8821e
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-27 13:06:23 +0000

    Changed README file

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/CryptoCrazeVisaCard.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/CryptoCrazeVisaCard.kt
@@ -1,1 +1,6 @@
-package com.nemesisprotocol.cryptocraze.do
+package com.nemesisprotocol.cryptocraze.domain.paymentinfo
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.nemesisprotocol.cryptocraze.presentation.walletscreen.C
