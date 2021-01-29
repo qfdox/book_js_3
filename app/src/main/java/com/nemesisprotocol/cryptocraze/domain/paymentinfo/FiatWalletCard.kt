3e244b0324b55a7f10c7758dc9a792e0fff6d350
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-29 22:56:06 +0000

    merge from 5.5 main

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/FiatWalletCard.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/FiatWalletCard.kt
@@ -1,3 +1,9 @@
 package com.nemesisprotocol.cryptocraze.domain.paymentinfo
 
-import andr
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "fiat_wallet_card_info")
+data class FiatWalletCard(
+    @Primar
