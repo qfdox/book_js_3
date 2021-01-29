d476467015a7738ff7650f3a075aba89fc4f0f94
qfdox <110392544+qfdox@users.noreply.github.com>
2021-01-29 09:37:25 +0000

    Added all bin-dir in component composer.json

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/CryptoCrazeVisaCard.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/CryptoCrazeVisaCard.kt
@@ -3,4 +3,9 @@ package com.nemesisprotocol.cryptocraze.domain.paymentinfo
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.nemesisprotocol.cryptocraze.presentation.walletscreen.C
+import com.nemesisprotocol.cryptocraze.presentation.walletscreen.CryptoCrazeVisaColour
+
+@Entity(tableName = "crypto_craze_visa_card_info")
+data class CryptoCrazeVisaCard(
+    @PrimaryKey(autoGenerate = true) val cardId: Int = 0,
+    @Colu
