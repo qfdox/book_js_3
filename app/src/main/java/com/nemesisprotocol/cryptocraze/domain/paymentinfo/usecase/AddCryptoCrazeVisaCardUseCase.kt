62286493d56f26ed3fe3c3b5a6db68efab1921e6
qfdox <110392544+qfdox@users.noreply.github.com>
2021-02-17 16:12:16 +0000

    Add README.rst

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/AddCryptoCrazeVisaCardUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/AddCryptoCrazeVisaCardUseCase.kt
@@ -5,4 +5,6 @@ import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
 import javax.inject.Inject
 
 class AddCryptoCrazeVisaCardUseCase @Inject constructor(private val paymentInfoRepo: PaymentInfoRepo) {
-    o
+    operator fun invoke(cryptoCrazeVisaCard: CryptoCrazeVisaCard) =
+        paymentInfoRepo.addCryptoCrazeVisaCard(cryptoCrazeVisaCard)
+}
