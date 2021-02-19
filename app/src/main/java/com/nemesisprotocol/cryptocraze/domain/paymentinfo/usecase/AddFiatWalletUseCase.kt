7cc0436db1f62742bfe910d7d807fd0f8c3ab50c
qfdox <110392544+qfdox@users.noreply.github.com>
2021-02-19 06:57:03 +0000

    version bumb

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/AddFiatWalletUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/AddFiatWalletUseCase.kt
@@ -1,1 +1,5 @@
-package com.nemesisprotocol.cryptocraze.
+package com.nemesisprotocol.cryptocraze.domain.paymentinfo.usecase
+
+import com.nemesisprotocol.cryptocraze.domain.paymentinfo.FiatWalletCard
+import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
+import javax.injec
