3c54c3e44951c5a8b588d63e4024aaf3d2e1e8e7
qfdox <110392544+qfdox@users.noreply.github.com>
2021-02-20 20:43:11 +0000

    Update Nepali translation

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/DeleteFiatWalletUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/DeleteFiatWalletUseCase.kt
@@ -1,4 +1,8 @@
 package com.nemesisprotocol.cryptocraze.domain.paymentinfo.usecase
 
 import com.nemesisprotocol.cryptocraze.domain.paymentinfo.FiatWalletCard
-import com.nemesisprotocol.crypt
+import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
+import javax.inject.Inject
+
+class DeleteFiatWalletUseCase @Inject constructor(private val paymentInfoRepo: PaymentInfoRepo) {
+    o
