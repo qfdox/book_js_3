5639b364bef05405d0fd3f4694f14596e4c1047f
qfdox <110392544+qfdox@users.noreply.github.com>
2021-02-21 18:04:33 +0000

    Release v0.0.2

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/GetCryptoCrazeVisaCardByIdUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/GetCryptoCrazeVisaCardByIdUseCase.kt
@@ -4,4 +4,7 @@ import com.nemesisprotocol.cryptocraze.domain.paymentinfo.CryptoCrazeVisaCard
 import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
 import javax.inject.Inject
 
-class GetCryptoCrazeVisaCardByIdUseCase @Inject constructor(private val pay
+class GetCryptoCrazeVisaCardByIdUseCase @Inject constructor(private val paymentInfoRepo: PaymentInfoRepo) {
+    operator fun invoke(cardId: Int): CryptoCrazeVisaCard =
+        paymentInfoRepo.getCryptoCrazeVisaCardByCardId(cardId)
+}
