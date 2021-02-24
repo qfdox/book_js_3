8794bc184e0145fc95d4a633231bd9c84921d040
qfdox <110392544+qfdox@users.noreply.github.com>
2021-02-24 12:56:21 +0000

    Refinements.

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/GetCryptoCrazeVisaCardsUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/GetCryptoCrazeVisaCardsUseCase.kt
@@ -1,4 +1,8 @@
 package com.nemesisprotocol.cryptocraze.domain.paymentinfo.usecase
 
 import com.nemesisprotocol.cryptocraze.domain.paymentinfo.CryptoCrazeVisaCard
-import com.nemesisprotocol.cryptocraze.domain.paymentinfo.Payment
+import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
+import javax.inject.Inject
+
+class GetCryptoCrazeVisaCardsUseCase @Inject constructor(private val paymentInfoRepo: PaymentInfoRepo) {
+    operator fun invoke(): List<
