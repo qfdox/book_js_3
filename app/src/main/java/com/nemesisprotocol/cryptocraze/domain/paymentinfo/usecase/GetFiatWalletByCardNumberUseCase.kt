2d781cad13aeed72916e1ff367d9ecbf3e176c71
qfdox <110392544+qfdox@users.noreply.github.com>
2021-02-25 22:25:18 +0000

    Release v0.0.9

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/GetFiatWalletByCardNumberUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/paymentinfo/usecase/GetFiatWalletByCardNumberUseCase.kt
@@ -4,4 +4,6 @@ import com.nemesisprotocol.cryptocraze.domain.paymentinfo.FiatWalletCard
 import com.nemesisprotocol.cryptocraze.domain.paymentinfo.PaymentInfoRepo
 import javax.inject.Inject
 
-class GetFiatWalletByCardNumberUseCase @Inject constructor(private val payment
+class GetFiatWalletByCardNumberUseCase @Inject constructor(private val paymentInfoRepo: PaymentInfoRepo) {
+    operator fun invoke(cardNumber: Long): FiatWalletCard =
+        paymentInfoRepo.getFiatWalletByCardNumber(cardNumber = cardNum
