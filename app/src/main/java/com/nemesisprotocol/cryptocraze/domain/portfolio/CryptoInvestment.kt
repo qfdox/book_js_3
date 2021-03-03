af3efb55f7c13430c9b0b6972f75cd0dfa7a9199
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-03 21:20:14 +0000

    added to .gitignore

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/portfolio/CryptoInvestment.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/portfolio/CryptoInvestment.kt
@@ -1,1 +1,7 @@
-package com.ne
+package com.nemesisprotocol.cryptocraze.domain.portfolio
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "crypto_
