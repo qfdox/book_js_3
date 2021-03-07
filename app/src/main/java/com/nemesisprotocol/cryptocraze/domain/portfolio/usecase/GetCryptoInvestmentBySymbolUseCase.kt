e2eaccbaa647e2b2ac935bac31fd60d3b9fc055f
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-07 10:52:02 +0000

    added markdown

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/portfolio/usecase/GetCryptoInvestmentBySymbolUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/portfolio/usecase/GetCryptoInvestmentBySymbolUseCase.kt
@@ -5,4 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class GetCryptoInvestmentBySymbolUseCase @Inject constructor(private val portfoli
+class GetCryptoInvestmentBySymbolUseCase @Inject constructor(private val portfolioRepo: PortfolioRepo) {
+    suspend operator fun invoke(cryptoSymbol: String) =
+        withContext(Dispatchers.Default) {
+            portfolioRepo.getCryptoInvestm
