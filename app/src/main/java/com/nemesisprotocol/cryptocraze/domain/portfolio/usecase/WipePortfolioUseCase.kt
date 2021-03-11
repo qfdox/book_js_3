8dbadecf471227d5843e1c07a0c56b41f7812a1b
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-11 14:12:56 +0000

    Update error message.

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/portfolio/usecase/WipePortfolioUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/portfolio/usecase/WipePortfolioUseCase.kt
@@ -1,3 +1,9 @@
 package com.nemesisprotocol.cryptocraze.domain.portfolio.usecase
 
-import com.nemesisprotocol.cryptocraze.domain.port
+import com.nemesisprotocol.cryptocraze.domain.portfolio.PortfolioRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class W
