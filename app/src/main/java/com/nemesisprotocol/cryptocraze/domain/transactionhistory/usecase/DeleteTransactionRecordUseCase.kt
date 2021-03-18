f6e6e25196cb97a5915dbf7b65e971e6e24b77db
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-18 11:45:11 +0000

    connector: improved unaligned access error fix

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/transactionhistory/usecase/DeleteTransactionRecordUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/transactionhistory/usecase/DeleteTransactionRecordUseCase.kt
@@ -1,3 +1,7 @@
 package com.nemesisprotocol.cryptocraze.domain.transactionhistory.usecase
 
-import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionHistory
+import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionHistoryRepo
+import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionRecord
+import javax.inject.Inject
+
+class DeleteTransactionRecordUseCase @Inject constructor(private val transac
