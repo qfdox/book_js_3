bd8d8fb9094fddca9257ba32c76e90120ac8dfd7
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-20 21:00:20 +0000

    Update Worksheet.md

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/transactionhistory/usecase/GetTransactionRecordsUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/transactionhistory/usecase/GetTransactionRecordsUseCase.kt
@@ -1,10 +1,8 @@
+
 package com.nemesisprotocol.cryptocraze.domain.transactionhistory.usecase
 
 import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionHistoryRepo
 import com.nemesisprotocol.cryptocraze.domain.transactionhistory.TransactionRecord
 import javax.inject.Inject
 
-class DeleteTransactionRecordUseCase @Inject constructor(private val transactionHistoryRepo: TransactionHistoryRepo) {
-    operator fun invoke(transactionRecord: TransactionRecord) =
-        transactionHistoryRepo.deleteTransactionRecord(transactionRecord)
-}
+class GetTransactionRecordsUseCase @Inject constructor(private val transactionHistoryRepo: TransactionHistoryRepo) {
