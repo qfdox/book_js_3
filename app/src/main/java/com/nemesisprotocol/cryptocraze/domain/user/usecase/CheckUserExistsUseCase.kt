58503e805d34d87f43cb422542012eb3ccc3a280
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-25 17:09:44 +0000

    add java doc

--- app/src/main/java/com/nemesisprotocol/cryptocraze/domain/user/usecase/CheckUserExistsUseCase.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/domain/user/usecase/CheckUserExistsUseCase.kt
@@ -1,4 +1,9 @@
 package com.nemesisprotocol.cryptocraze.domain.user.usecase
 
 import com.nemesisprotocol.cryptocraze.domain.user.UserRepo
-import kotlin
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CheckUserExistsUseCase @Inject constructor(private val userRepo: UserRepo) {
+    suspend
