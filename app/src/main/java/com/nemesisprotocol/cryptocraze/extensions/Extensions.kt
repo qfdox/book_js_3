d4e7de413bf87e25e56ae7655f85056a877d8b37
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-27 09:47:51 +0000

    Spellcheck

--- app/src/main/java/com/nemesisprotocol/cryptocraze/extensions/Extensions.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/extensions/Extensions.kt
@@ -5,4 +5,4 @@ import java.text.DecimalFormat
 private val twoDecimalFormat = DecimalFormat("##.##")
 private val threeDecimalFormat = DecimalFormat("##.###")
 fun Double.roundToTwoDecimals() = twoDecimalFormat.format(this).toString()
-fun Double.roundToThreeDecimals() = threeDecimalFor
+fun Double.roundToThreeDecimals() = threeDecimalFormat.format(this).toString()
---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/BottomNavigationBar.kt
@@ -0,0 +1,5 @@
+package com.nemesisprotocol.cryptocraze.presentation
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.ru
