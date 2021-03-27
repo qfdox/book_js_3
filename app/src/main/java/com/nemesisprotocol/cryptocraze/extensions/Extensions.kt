f5195ca6062f3aa2f2cd2043b2a45a5f0e495f75
qfdox <110392544+qfdox@users.noreply.github.com>
2021-03-27 08:53:57 +0000

    Versao inicial do artefato

--- app/src/main/java/com/nemesisprotocol/cryptocraze/extensions/Extensions.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/extensions/Extensions.kt
@@ -2,4 +2,7 @@ package com.nemesisprotocol.cryptocraze.extensions
 
 import java.text.DecimalFormat
 
-private val twoDecimalFormat = DecimalFormat
+private val twoDecimalFormat = DecimalFormat("##.##")
+private val threeDecimalFormat = DecimalFormat("##.###")
+fun Double.roundToTwoDecimals() = twoDecimalFormat.format(this).toString()
+fun Double.roundToThreeDecimals() = threeDecimalFor
