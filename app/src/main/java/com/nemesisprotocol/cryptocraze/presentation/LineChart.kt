a32cd466daa9b803eaa8786e6a48df29fe335872
qfdox <110392544+qfdox@users.noreply.github.com>
2021-04-20 11:47:56 +0000

    Modified config

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/LineChart.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/LineChart.kt
@@ -35,4 +35,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+import kotlin.mat
