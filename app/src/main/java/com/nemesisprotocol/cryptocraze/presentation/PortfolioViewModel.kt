7284ed2b055732cb9dc9f5ba47213eb9af9c3a6a
qfdox <110392544+qfdox@users.noreply.github.com>
2021-05-03 10:18:36 +0000

    Removed var_dump

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/PortfolioViewModel.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/PortfolioViewModel.kt
@@ -4,4 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nemesisprotocol.cryptocraze.common.DispatcherProvider
 import com.nemesisprotocol.cryptocraze.domain.portfolio.CryptoInvestment
-import com.nemesisprotocol.cryptocraze.domain.portfolio
+import com.nemesisprotocol.cryptocraze.domain.portfolio.usecase.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
