6285a3448b6ed23b2f7362839daed7be2460d7be
qfdox <110392544+qfdox@users.noreply.github.com>
2021-05-03 15:10:47 +0000

    Fixes syntax

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/PortfolioViewModel.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/PortfolioViewModel.kt
@@ -9,3 +9,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class PortfolioViewModel @Inject constructor(
+    private val addCryptoInvestmentUseCa
