5b5ce57be6a29697e6c4cd1c2560209f657cb9c2
qfdox <110392544+qfdox@users.noreply.github.com>
2021-05-09 11:50:38 +0000

    sctp: sctp should release assoc when sctp_make_abort_user return NULL in sctp_close

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/aboutscreen/AboutScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/aboutscreen/AboutScreen.kt
@@ -2,4 +2,7 @@ package com.nemesisprotocol.cryptocraze.presentation.aboutscreen
 
 import android.content.Intent
 import androidx.compose.material.Text
-import androidx.compose.r
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.alorma.compose.settings.ui.SettingsMenuLink
+import com.google.android.gms.oss.licenses.OssLic
