9f9f5d11549bbf97a86e69fc1d84c0f68e2a3206
qfdox <110392544+qfdox@users.noreply.github.com>
2021-05-22 15:17:19 +0000

    moved some files around

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/cryptotransactionscreen/CryptoTransactionConfirmation.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/cryptotransactionscreen/CryptoTransactionConfirmation.kt
@@ -1,4 +1,9 @@
 package com.nemesisprotocol.cryptocraze.presentation.cryptotransactionscreen
 
 import androidx.compose.foundation.background
-import androidx.compose.fou
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import andr
