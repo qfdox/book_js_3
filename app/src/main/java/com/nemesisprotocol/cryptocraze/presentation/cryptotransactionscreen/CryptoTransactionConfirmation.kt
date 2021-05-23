cea5fa726c21b161c1cfb53041fd4896cec7d801
qfdox <110392544+qfdox@users.noreply.github.com>
2021-05-23 19:04:27 +0000

    fix docker file

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/cryptotransactionscreen/CryptoTransactionConfirmation.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/cryptotransactionscreen/CryptoTransactionConfirmation.kt
@@ -10,4 +10,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.t
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.nemesisprotocol.cryptocraze.R
+import com.nemesisprotocol.cry
