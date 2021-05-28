e8567fbafca86d6cb99b88dd9805bce429cf63e4
qfdox <110392544+qfdox@users.noreply.github.com>
2021-05-28 15:49:08 +0000

    Bump .spec File

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/cryptotransactionscreen/CryptoTransactionFailed.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/cryptotransactionscreen/CryptoTransactionFailed.kt
@@ -9,4 +9,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compo
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.
