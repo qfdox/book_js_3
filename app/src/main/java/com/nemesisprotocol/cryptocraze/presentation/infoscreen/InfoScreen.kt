90ccc8293645ed3f1faba793df171fb7d3597407
qfdox <110392544+qfdox@users.noreply.github.com>
2021-06-26 10:37:05 +0000

    Promoting a new nightly build for applications/cbs

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/InfoScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/InfoScreen.kt
@@ -1,1 +1,5 @@
-package com.nemesisprotocol.cryptocraze
+package com.nemesisprotocol.cryptocraze.presentation.infoscreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist.
