5f0386ee1a5ead81a641f27ec919ae0925514585
qfdox <110392544+qfdox@users.noreply.github.com>
2021-06-27 22:20:21 +0000

    Update data.js

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoScreen.kt
@@ -1,1 +1,6 @@
-package com.nemesisprotocol.cryptocraze.presentation.i
+package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfo
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compos
