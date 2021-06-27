ca6a7965281bf27c8806860d97d7f5649c76735b
qfdox <110392544+qfdox@users.noreply.github.com>
2021-06-27 10:21:04 +0000

    upgrade dev dependencies

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/InfoScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/InfoScreen.kt
@@ -2,4 +2,9 @@ package com.nemesisprotocol.cryptocraze.presentation.infoscreen
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist.
+import com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist.CoinInfoListScreen
+
+@Composable
+fun InfoScreen(navController: NavController) {
+    CoinInfoListScreen(navController = navController)
+}
