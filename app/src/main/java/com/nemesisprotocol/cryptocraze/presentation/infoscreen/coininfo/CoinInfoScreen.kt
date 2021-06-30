cdad630712881143b734b563681e1d76d6277e97
qfdox <110392544+qfdox@users.noreply.github.com>
2021-06-30 21:10:59 +0000

    update phpunit

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoScreen.kt
@@ -3,4 +3,8 @@ package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfo
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compos
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+im
