f43b45415156cda04d32802dd498cfb2873cb535
qfdox <110392544+qfdox@users.noreply.github.com>
2021-07-04 13:26:35 +0000

    package - version bump

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoScreen.kt
@@ -7,4 +7,8 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-im
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import andro
