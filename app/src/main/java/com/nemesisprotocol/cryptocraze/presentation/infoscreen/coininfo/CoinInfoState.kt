352637c8d3204e87da5457a189593c7ef771436c
qfdox <110392544+qfdox@users.noreply.github.com>
2021-07-24 15:31:08 +0000

    fixed classpath

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoState.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfo/CoinInfoState.kt
@@ -1,1 +1,7 @@
-package com.nemesisprotocol.cryptoc
+package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfo
+
+import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.CoinDetail
+
+data class CoinInfoState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail?
