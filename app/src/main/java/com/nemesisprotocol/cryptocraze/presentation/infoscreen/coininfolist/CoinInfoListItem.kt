3c6e99550aac19af436a174d20c171eb12ae5222
qfdox <110392544+qfdox@users.noreply.github.com>
2021-07-25 13:36:44 +0000

    expression

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListItem.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListItem.kt
@@ -2,4 +2,8 @@ package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.
