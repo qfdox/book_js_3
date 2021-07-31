6f4ae843e0440464e14e6c6e4b994dd50befa943
qfdox <110392544+qfdox@users.noreply.github.com>
2021-07-31 15:42:14 +0000

    modify article

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListScreen.kt
@@ -1,4 +1,7 @@
 package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist
 
 import androidx.compose.foundation.layout.*
-im
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx
