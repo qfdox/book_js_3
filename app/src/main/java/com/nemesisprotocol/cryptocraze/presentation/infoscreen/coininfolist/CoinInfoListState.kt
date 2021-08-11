d70fc9836dd38a413d5f4317f2265fed94a226c3
qfdox <110392544+qfdox@users.noreply.github.com>
2021-08-11 14:01:43 +0000

    chore(package): update lodash to version 4.13.1

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListState.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListState.kt
@@ -1,3 +1,9 @@
 package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist
 
-import com.nemesisprotocol.cryptocraze.do
+import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.Coin
+
+data class CoinInfoListState(
+    val isLoading: Boolean = false,
+    val coins: List<Coin> = emptyList(),
+    val error: String = ""
+)
