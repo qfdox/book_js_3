4ad157e66546aa549559381d2c6edd2422428cd7
qfdox <110392544+qfdox@users.noreply.github.com>
2021-08-18 20:07:10 +0000

    l2tp: add missing .owner to struct pppox_proto

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListViewModel.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListViewModel.kt
@@ -3,4 +3,7 @@ package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import android
+import androidx.lifecycle.viewModelScope
+import com.nemesisprotocol.cryptocraze.common.Resource
+import com.nemesisprotocol.cryptocraze.domain.cryptoinfo.usecase.GetCoinsUseCase
+import dagger.hilt.android.lifecycle.Hilt
