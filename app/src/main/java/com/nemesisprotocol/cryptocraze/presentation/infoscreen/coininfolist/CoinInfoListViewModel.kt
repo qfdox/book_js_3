306cc262042b055a602f3f462a68ab621090edaf
qfdox <110392544+qfdox@users.noreply.github.com>
2021-08-18 07:23:23 +0000

    Ver bump

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListViewModel.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/infoscreen/coininfolist/CoinInfoListViewModel.kt
@@ -1,1 +1,6 @@
-package com.nemesisprotocol.cry
+package com.nemesisprotocol.cryptocraze.presentation.infoscreen.coininfolist
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import android
