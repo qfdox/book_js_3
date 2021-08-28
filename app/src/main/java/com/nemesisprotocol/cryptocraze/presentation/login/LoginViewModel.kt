fde3888b805824cddc15e401d4c5cc5ec9af6eaa
qfdox <110392544+qfdox@users.noreply.github.com>
2021-08-28 15:23:09 +0000

    add protobuf

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/login/LoginViewModel.kt
@@ -1,1 +1,5 @@
-package com.nemesisprotocol.cryptocraze.presentation
+package com.nemesisprotocol.cryptocraze.presentation.login
+
+import androidx.lifecycle.ViewModel
+import com.nemesisprotocol.cryptocraze.common.DispatcherProvider
+import com.nemesisprotocol.cryptocraze.domain.user.Us
