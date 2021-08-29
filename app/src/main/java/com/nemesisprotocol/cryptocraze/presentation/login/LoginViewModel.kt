2191e1c8bc26594e0b467abf7ec0552a25b8aae9
qfdox <110392544+qfdox@users.noreply.github.com>
2021-08-29 17:33:22 +0000

    Update SoftwareToInstall.md

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/login/LoginViewModel.kt
@@ -2,4 +2,8 @@ package com.nemesisprotocol.cryptocraze.presentation.login
 
 import androidx.lifecycle.ViewModel
 import com.nemesisprotocol.cryptocraze.common.DispatcherProvider
-import com.nemesisprotocol.cryptocraze.domain.user.Us
+import com.nemesisprotocol.cryptocraze.domain.user.User
+import com.nemesisprotocol.cryptocraze.domain.user.usecase.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.withContext
+import javax.inject.Inj
