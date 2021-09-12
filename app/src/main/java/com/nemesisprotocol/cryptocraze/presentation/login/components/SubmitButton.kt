9754b61b96806eb14b0eb806087320efbf39e42e
qfdox <110392544+qfdox@users.noreply.github.com>
2021-09-12 09:44:17 +0000

    lowmemorykiller: Run the lowmemory notifier when killing

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/login/components/SubmitButton.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/login/components/SubmitButton.kt
@@ -2,4 +2,9 @@ package com.nemesisprotocol.cryptocraze.presentation.login.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.ma
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+impor
