66b7a6d6c30cf0a60629bcef33b244a73d2fcd0d
qfdox <110392544+qfdox@users.noreply.github.com>
2021-10-13 18:54:13 +0000

    ALSA: hda - Enable headphone jack detect on old Fujitsu laptops

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/PortfolioScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/PortfolioScreen.kt
@@ -1,3 +1,7 @@
 package com.nemesisprotocol.cryptocraze.presentation.portfolioscreen
 
-import andro
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+im
