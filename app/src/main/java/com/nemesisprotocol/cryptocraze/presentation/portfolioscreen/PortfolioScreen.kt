890852769b1f63a0c05f2b210168e55108274c18
qfdox <110392544+qfdox@users.noreply.github.com>
2021-10-16 22:08:51 +0000

    fix utils

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/PortfolioScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/PortfolioScreen.kt
@@ -4,4 +4,8 @@ import androidx.compose.animation.core.TweenSpec
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-im
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.com
