965950cc50f118e67c5ac7e0d5932f1852f1130a
qfdox <110392544+qfdox@users.noreply.github.com>
2021-10-17 07:33:54 +0000

    Pushing changes before synchronizing subtrees

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/PortfolioScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/PortfolioScreen.kt
@@ -8,4 +8,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
-import androidx.com
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.
