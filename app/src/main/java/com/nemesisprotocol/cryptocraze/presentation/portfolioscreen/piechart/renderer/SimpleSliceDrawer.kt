9b9fcf10f136de1b8bf0a9a9638ecdb387b888dc
qfdox <110392544+qfdox@users.noreply.github.com>
2021-12-26 18:38:58 +0000

    added new project

---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/piechart/renderer/SimpleSliceDrawer.kt
@@ -0,0 +1,81 @@
+
+/*
+* Copyright 2020 Taras Koshkin.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package com.nemesisprotocol.cryptocraze.presentation.portfolioscreen.piechart.renderer
+
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import com.nemesisprotocol.cryptocraze.presentation.portfolioscreen.piechart.PieChartData.Slice
+
+class SimpleSliceDrawer(private val sliceThickness: Float = 25f) : SliceDrawer {
+    init {
+        require(sliceThickness in 10f..100f) {
+            "Thickness of $sliceThickness must be between 10-100"
+        }
+    }
+
+    private val sectionPaint = Paint().apply {
+        isAntiAlias = true
+        style = PaintingStyle.Stroke
+    }
+
+    override fun drawSlice(
+        drawScope: DrawScope,
+        canvas: Canvas,
+        area: Size,
+        startAngle: Float,
+        sweepAngle: Float,
+        slice: Slice
+    ) {
+        val sliceThickness = calculateSectorThickness(area = area)
+        val drawableArea = calculateDrawableArea(area = area)
+
+        canvas.drawArc(
+            rect = drawableArea,
+            paint = sectionPaint.apply {
+                color = slice.color
+                strokeWidth = sliceThickness
+            },
+            startAngle = startAngle,
+            sweepAngle = sweepAngle,
+            useCenter = false
+        )
+    }
+
+    private fun calculateSectorThickness(area: Size): Float {
+        val minSize = minOf(area.width, area.height)
+
+        return minSize * (sliceThickness / 200f)
+    }
+
+    private fun calculateDrawableArea(area: Size): Rect {
+        val sliceThicknessOffset =
+            calculateSectorThickness(area = area) / 2f
+        val offsetHorizontally = (area.width - area.height) / 2f
+
+        return Rect(
+            left = sliceThicknessOffset + offsetHorizontally,
+            top = sliceThicknessOffset,
+            right = area.width - sliceThicknessOffset - offsetHorizontally,
+            bottom = area.height - sliceThicknessOffset
+        )
+    }
+}
---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/portfolioscreen/piechart/renderer/SliceDrawer.kt
@@ -0,0 +1,4 @@
+/*
+* Copyright 2020 Taras Koshkin.
+*
+* Licensed under the Apache License, Version 2.0 (the "License
