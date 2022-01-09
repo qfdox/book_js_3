e689a38134a90a380748865f3009050a9262bf2a
qfdox <110392544+qfdox@users.noreply.github.com>
2022-01-09 12:12:01 +0000

    added stylesheet

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/splashscreen/SplashScreen.kt
@@ -11,4 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.nemesisprotocol.cryptocraze.R
+import com.nemesisprotocol.cry
