65d687ccdb36281a4454d93b08652af3b57a0c9d
qfdox <110392544+qfdox@users.noreply.github.com>
2022-01-15 07:01:58 +0000

    Fix Markdown syntax

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/splashscreen/SplashScreen.kt
@@ -16,4 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.nemesisprotocol.cryptocraze.R
-import com.nemesisprotocol.cry
+import com.nemesisprotocol.cryptocraze.Screen
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedE
