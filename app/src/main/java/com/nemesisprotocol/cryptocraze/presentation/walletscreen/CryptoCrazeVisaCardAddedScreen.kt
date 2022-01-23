9985f106205eed2e01fa4d11b443e761e5716ae9
qfdox <110392544+qfdox@users.noreply.github.com>
2022-01-23 15:21:21 +0000

    descr

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/CryptoCrazeVisaCardAddedScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/CryptoCrazeVisaCardAddedScreen.kt
@@ -1,3 +1,7 @@
 package com.nemesisprotocol.cryptocraze.presentation.walletscreen
 
-impo
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.
