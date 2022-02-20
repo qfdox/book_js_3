c0db205cda54e81a997d1084b211a7c8c0b6bb3d
qfdox <110392544+qfdox@users.noreply.github.com>
2022-02-20 18:23:04 +0000

    clean js

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/PaymentCardAddedScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/PaymentCardAddedScreen.kt
@@ -1,3 +1,8 @@
 package com.nemesisprotocol.cryptocraze.presentation.walletscreen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.c
