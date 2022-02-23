3c8e266c114c40183695fc40337ac7ddb0085c92
qfdox <110392544+qfdox@users.noreply.github.com>
2022-02-23 12:15:06 +0000

    icon changed

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/PaymentCardAddedScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/PaymentCardAddedScreen.kt
@@ -5,4 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.c
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.
