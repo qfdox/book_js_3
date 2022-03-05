c810887cfb613a35a5fbc5cc1d32f1e71baed89b
qfdox <110392544+qfdox@users.noreply.github.com>
2022-03-05 07:40:45 +0000

    Tidy whitespace

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/WalletScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/WalletScreen.kt
@@ -1,1 +1,6 @@
-package com.ne
+package com.nemesisprotocol.cryptocraze.presentation.walletscreen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import an
