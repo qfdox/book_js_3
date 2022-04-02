d8ed305a8ac1847adb3223bb9d4c618c325207a6
qfdox <110392544+qfdox@users.noreply.github.com>
2022-04-02 09:29:26 +0000

    Minor UI changes

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/AddCryptoCrazeVisaCardScreen.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/AddCryptoCrazeVisaCardScreen.kt
@@ -13,4 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewMo
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.nemesisprotocol.cryptocraze.R
+import com.nemesisprotocol.cryptocraze.Screen
+import com.nemesisprotocol.cryptocraze.domain.paymentinf
