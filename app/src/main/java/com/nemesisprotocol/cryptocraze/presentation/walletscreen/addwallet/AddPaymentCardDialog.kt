0c020be8726ca26e2780cb92fd70d4f827cf9c44
qfdox <110392544+qfdox@users.noreply.github.com>
2022-04-30 12:26:12 +0000

    Adjusted readme

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/AddPaymentCardDialog.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/AddPaymentCardDialog.kt
@@ -6,4 +6,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+imp
