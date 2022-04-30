6a3482370975c0cd853211d21efaab8e5d3da401
qfdox <110392544+qfdox@users.noreply.github.com>
2022-04-30 08:01:58 +0000

    upnp fix

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/AddPaymentCardDialog.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/AddPaymentCardDialog.kt
@@ -2,4 +2,8 @@ package com.nemesisprotocol.cryptocraze.presentation.walletscreen.addwallet
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fill
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.
