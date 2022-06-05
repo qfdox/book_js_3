4a4021952ae19a5c071667840bf16f3b40d2764c
qfdox <110392544+qfdox@users.noreply.github.com>
2022-06-05 19:36:47 +0000

    add some more tests

--- app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/CryptoCrazeVisaCard.kt
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/CryptoCrazeVisaCard.kt
@@ -10,3 +10,95 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.nemesisprotocol.cryptocraze.R
+import com.nemesisprotocol.cryptocraze.presentation.walletscreen.CryptoCrazeVisaColour
+
+@ExperimentalAnimationApi
+@Composable
+fun CryptoCrazeVisaCard(cardColour: MutableState<CryptoCrazeVisaColour>) {
+    val animatedColor = animateColorAsState(
+        targetValue =
+        when (cardColour.value) {
+            CryptoCrazeVisaColour.BLACK -> Color(0xFF000000)
+            CryptoCrazeVisaColour.WHITE -> Color(0xFFFFFFFF)
+            CryptoCrazeVisaColour.RED -> Color(0xFFB53737)
+            CryptoCrazeVisaColour.GREEN -> Color(0xFF26580F)
+            CryptoCrazeVisaColour.BLUE -> Color(0xFF003166)
+            CryptoCrazeVisaColour.SILVER -> Color(0xFFC0C0C0)
+        }
+    )
+    Box(
+        modifier = Modifier.padding(top = 16.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(220.dp),
+            shape = RoundedCornerShape(25.dp),
+            color = animatedColor.value,
+            elevation = 18.dp
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                ConstraintLayout(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    val (symbol, logo, number) = createRefs()
+
+                    Image(
+                        painter = painterResource(
+                            id = R.drawable.card_symbol
+                        ),
+                        contentDescription = "symbol",
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .constrainAs(symbol) {
+                                start.linkTo(parent.start)
+                                top.linkTo(parent.top)
+                            }
+                    )
+
+                    AnimatedVisibility(
+                        visible = true,
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .constrainAs(logo) {
+                                end.linkTo(parent.end)
+                                top.linkTo(parent.top)
+                            }
+                    ) {
+                        Image(
+                            painter = painterResource(
+                                id = R.drawable.visa_logo
+                            ),
+                            contentDescription = "symbol"
+                        )
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.cryptocraze_icon),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .animateContentSize(spring())
+                            .padding(vertical = 16.dp, horizontal = 16.dp)
+                            .constrainAs(number) {
+                                linkTo(
+                                    start = parent.start,
+                                    end = parent.end
+                                )
+                                linkTo(
+                                    top = parent.top,
+                                    bottom = parent.bottom
+                                )
+                            }
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/nemesisprotocol/cryptocraze/presentation/walletscreen/addwallet/InputItem.kt
@@ -0,0 +1,70 @@
+
+/*
+* MIT License
+*
+* Copyright (c) 2020 Gurupreet Singh
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ */
+
+package com.nemesisprotocol.cryptocraze.presentation.walletscreen.addwallet
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun InputItem(
+    textFieldValue: TextFieldValue,
+    label: String,
+    onTextChanged: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    textStyle: TextStyle = MaterialTheme.typography.body1,
+    keyboardType: KeyboardType = KeyboardType.Text
+) {
+    OutlinedTextField(
+        value = textFieldValue,
+        onValueChange = { onTextChanged(it) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = ImeAction.Next
+        ),
+        textStyle = textStyle,
+        maxLines = 1,
+        singleLine = true,
+        label = {
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        },
+        modifier = modifier,
+        visualTransformation = visualTransformation
+    )
+}
