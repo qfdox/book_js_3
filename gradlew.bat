47d7dd9c8f2d7cb7788ee4ead002f5fe21dc572d
qfdox <110392544+qfdox@users.noreply.github.com>
2023-02-17 13:09:10 +0000

    Fix spell error

--- gradlew.bat
+++ gradlew.bat
@@ -26,4 +26,10 @@ if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-set AP
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS a
