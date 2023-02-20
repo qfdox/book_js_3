a02e564e8976a991253bd9d77df31547716157f5
qfdox <110392544+qfdox@users.noreply.github.com>
2023-02-20 06:18:55 +0000

    add license files

--- gradlew.bat
+++ gradlew.bat
@@ -52,4 +52,11 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.ex
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your e
